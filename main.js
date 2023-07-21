997bc23570b3787f68039cd1331457d4a44cfc95
Bikrom-Roy <bikromroy2001@gmail.com>
2023-07-21 23:30:24 +0600

    image gallery

---
+++ main.js
@@ -0,0 +1,20 @@
+const scrollContainer = document.querySelector(".Gallery");
+
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (event) => {
+  event.preventDefault();
+  scrollContainer.scrollLeft += event.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
